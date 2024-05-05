e294b11860aa56458f5d6d96575ab24fb28f713e
Hamza Saleem <saleemh075@gmail.com>
2024-05-05 23:38:27 +0500

    add About,Navbar,Home Component

---
+++ src/components/About.js
@@ -0,0 +1,8 @@
+import React, { useContext, useEffect } from "react";
+// import noteContext from '../context/notes/noteContext'
+
+const About = () => {
+  return <div>This is about Page Section</div>;
+};
+
+export default About;
