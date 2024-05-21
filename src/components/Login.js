2736236a50a2b2a09e8200c2453906840dbb0a84
Hamza Saleem <saleemh075@gmail.com>
2024-05-21 17:57:10 +0500

    implement signup functionality

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,74 @@
-import React from 'react'
+import React, {useState} from "react";
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
-  return (
-    <div>Login Page</div>
-  )
-}
+  let navigate = useNavigate();
+  const [credentials,setCredentials] = useState({email:"" , password:""})
+  const host = "http://localhost:5000";
+  const  handleSubmit = async (e) => {
+    e.preventDefault();
+    const response = await fetch(`${host}/api/auth/login`, {
+      method: "POST", 
+      headers: {
+        "Content-Type": "application/json",
+        "auth-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjYzMGQ3NTA1NTlhMjlhMDc4MTJlMGIxIn0sImlhdCI6MTcxNDU1OTQ1NX0.fZLUEYuvqoCqzCYDXaCXgKVpuPFajVc1SzoVcxm6aRY",
+      }, 
+      body: JSON.stringify({email:credentials.email, password:credentials.password}), 
+    });
+    const json = await response.json()
+    console.log(json)
+    if(json.success){
+      //save the token in localstorage and redirect
+      localStorage.setItem('token',json.authToken)
+      navigate('/');
+    }
+    else{
+      alert("Invalid Credentials")
+    }
+  }
 
-export default Login
+  const onChange = (e) =>{
+    setCredentials({...credentials, [e.target.name]: e.target.value})
+  }
+  return (
+    <div>
+      <form  onSubmit={handleSubmit}>
+        <div className="mb-3">
+          <label htmlFor="email" className="form-label">
+            Email address
+          </label>
+          <input
+            type="email"
+            className="form-control"
+            id="email"
+            aria-describedby="emailHelp"
+            name="email"
+            value={credentials.email}
+            onChange={onChange}
+          />
+          <div id="emailHelp" className="form-text">
+            We'll never share your email with anyone else.
+          </div>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="password" className="form-label">
+            Password
+          </label>
+          <input
+            type="password"
+            className="form-control"
+            id="password"
+            name="password"
+            value={credentials.password}
+            onChange={onChange}
+          />
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,9 +1,100 @@
-import React from 'react'
+import React, {useState} from "react";
+import { useNavigate } from 'react-router-dom';
+
 
 const Signup = () => {
-  return (
-    <div>Signup Page</div>
-  )
-}
+  const [credentials,setCredentials] = useState({name:"" , email:"" , password:"" , cpassword:""})
+  let navigate = useNavigate();
+  const  handleSubmit = async (e) => {
+    e.preventDefault();
+    const {name,email,password} = credentials;
+    const response = await fetch(`http://localhost:5000/api/auth/createuser`, {
+      method: "POST", 
+      headers: {
+        "Content-Type": "application/json",
+      }, 
+      body: JSON.stringify({name,email,password})
+    });
+    const json = await response.json()
+    console.log(json)
+    if(json.success){
+      //save the token in localstorage and redirect
+      localStorage.setItem('token',json.authToken)
+      navigate('/');
+    }
+    else{
+      alert("Invalid Credentials")
+    }
+  }
 
-export default Signup
+  const onChange = (e) =>{
+    setCredentials({...credentials, [e.target.name]: e.target.value})
+  }
+  return (
+    <div className="container">
+      <form onSubmit={handleSubmit}>
+        <div className="mb-3">
+          <label htmlFor="name" className="form-label">
+            Name
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="name"
+            name="name"
+            onChange={onChange}
+            aria-describedby="emailHelp"
+            required
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="email" className="form-label">
+            Email address
+          </label>
+          <input
+            type="email"
+            className="form-control"
+            id="email"
+            name="email"
+            onChange={onChange}
+            aria-describedby="emailHelp"
+            required
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="Password" className="form-label">
+            Password
+          </label>
+          <input
+            type="password"
+            className="form-control"
+            id="Password"
+            name="password"
+            onChange={onChange}
+            minLength={5}
+            required
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="cpassword" className="form-label">
+            Confirm Password
+          </label>
+          <input
+            type="password"
+            className="form-control"
+            id="cpassword"
+            name="cpassword"
+            onChange={onChange}
+            minLength={5}
+            required
+          />
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Signup;
